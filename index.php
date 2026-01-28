a2ed8cd18dd4acd3c12fb3e7d8f09cbabcf1eacf
Tirthgoklani <tirthgoklani2580@gmail.com>
2026-01-29 00:10:57 +0530

    Enable error reporting for debugging

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 // index.php
 require_once 'config/config.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Function to sanitize input
 function sanitize_input($data) {
