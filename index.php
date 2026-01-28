3f52a71658848de8d79d6ee3576d7b63594a8b13
Tirthgoklani <tirthgoklani2580@gmail.com>
2026-01-29 00:12:35 +0530

    Move error reporting to top

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 // index.php
-require_once 'config/config.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once 'config/config.php';
 
 // Function to sanitize input
 function sanitize_input($data) {
