71fd9958115d896a21a715a1dc3da7a07a54493f
Tirthgoklani <tirthgoklani2580@gmail.com>
2026-02-09 23:33:12 +0530

    Add error display for debugging export issues

--- src/controllers/export_invoices.php
+++ src/controllers/export_invoices.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error display for debugging (remove after fixing)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once '../../config/config.php';
 
 // Dompdf for PDF generation
